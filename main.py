4c28580dd5efac1cebf02093e69d78af2c0755f2
Mahmoud Sayed <mahmoud.a.elsayed97@outlook.com>
2023-12-13 07:12:19 +0200

    get the contact page to work

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,request
 import requests
+import smtplib
+import os
+USER_NAME = os.environ["EMAIL"]
+PASSWORD = os.environ["PASS"]
 app = Flask(__name__)
 
 response = requests.get(url="https://api.npoint.io/de09e0e706dadd0e0e6d")
@@ -9,9 +13,23 @@ data = response.json()
 def home():
     return render_template("index.html",data = data)
 
-@app.route("/contact")
+@app.route("/contact",methods=["POST","GET"])
 def contact():
-    return render_template("contact.html")
+    change = True
+    if request.method == "POST":
+        connection = smtplib.SMTP("smtp.gmail.com",587)
+        connection.starttls()
+        connection.login(user=USER_NAME,password=PASSWORD)
+        connection.sendmail(to_addrs=USER_NAME,from_addr=USER_NAME,msg=f"Subject: Fan Message \n\n\n"
+                                                                       f"You got a message from: {request.form['name']}\n"
+                                                                       f"Mail: {request.form['email']}\n"
+                                                                       f"Phone: {request.form['phone']}\n"
+                                                                       f"Content: {request.form['message']} ")
+        connection.close()
+        change = False
+        return  render_template("contact.html",change = change)
+    else :
+        return render_template("contact.html",change = change)
 
 @app.route("/about")
 def about():
@@ -22,6 +40,10 @@ def blog(id):
     response = requests.get(url="https://api.npoint.io/de09e0e706dadd0e0e6d")
     data = response.json()
     return render_template("post.html",post=data[int(id)-1])
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
